95160b98c04427a5975236fca19bb0a284e4f7e8
kamaal111 <kamaal.f1@gmail.com>
2022-01-16 22:46:25 +0100

    Adding a example client app target

---
+++ Example/Shared/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Shared
+//
+//  Created by Kamaal M Farah on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
